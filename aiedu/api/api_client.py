86005fe2574eab3a42e0bd3ac995c077fe633bdb
martar27 <125350158+martar27@users.noreply.github.com>
2024-03-27 13:20:53 +0200

    Add api_client.py for API interactions

---
+++ aiedu/api/api_client.py
@@ -0,0 +1,13 @@
+# api_client.py
+
+class APIClient:
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def send_request(self, data):
+        # Logic to send a request to the API
+        pass
+
+    def receive_response(self):
+        # Logic to handle the response from the API
+        pass
