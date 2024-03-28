ce96019c2a6c1c74095bc254eb470d62366be333
martar27 <125350158+martar27@users.noreply.github.com>
2024-03-28 23:34:23 +0200

    Update database.py

--- aiedu/database/database.py
+++ aiedu/database/database.py
@@ -1,5 +1,5 @@
 # database.py
-"""This module will handle the database connections and ORM models."""
+
 import duckdb
 from datetime import datetime
 
@@ -11,18 +11,22 @@ class DatabaseManager:
 
     def create_connection(self):
         # Establish a connection to the database and create the user_inputs table if it doesn't exist
-        self.conn = duckdb.connect(database=self.database_path, read_only=False)
-        self.conn.execute("""
-            CREATE TABLE IF NOT EXISTS user_inputs (
-                user_id INTEGER,
-                input_text TEXT,
-                is_llm_response BOOLEAN,
-                timestamp TIMESTAMP
-            )
-        """)
+        if not self.conn:
+            self.conn = duckdb.connect(database=self.database_path, read_only=False)
+            self.conn.execute("""
+                CREATE TABLE IF NOT EXISTS user_inputs (
+                    user_id INTEGER,
+                    input_text TEXT,
+                    is_llm_response BOOLEAN,
+                    timestamp TIMESTAMP
+                )
+            """)
 
     def log_input_to_database(self, user_id, input_text, is_llm_response=False):
         # Log user inputs or LLM responses to the database
+        if not self.conn:
+            self.create_connection()
+            
         timestamp = datetime.now().isoformat()
         self.conn.execute(
             "INSERT INTO user_inputs (user_id, input_text, is_llm_response, timestamp) VALUES (?, ?, ?, ?)",
@@ -33,3 +37,4 @@ class DatabaseManager:
         # Logic to close the database connection
         if self.conn:
             self.conn.close()
+            self.conn = None
