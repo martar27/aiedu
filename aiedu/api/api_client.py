8977148ccfeed969edd935d6e3eeb9c4c53f6a7e
martar27 <marti.taru@ut.ee>
2024-07-26 13:09:24 +0300

    api_call

--- aiedu/api/api_client.py
+++ aiedu/api/api_client.py
@@ -12,6 +12,7 @@ class APIClient:
         # Initsialiseeri API key keskkonna muutujast OPENAI_API_KEY
         self.api_key = os.getenv('OPENAI_API_KEY') 
         openai.api_key = self.api_key
+        self.student_user_ids = ["kasutaja1", "kasutaja2", "kasutaja3"] # initial list of student user ids. this object is dyamic and will be updated with the actual list of student user ids from the database. currently it is a hard-coded list.
         #self.db_manager = db_manager  # DatabaseManager instance
         #db_path = os.getenv('DATABASE_PATH')
         #self.db_manager = DatabaseManager(db_path)
@@ -49,10 +50,13 @@ class APIClient:
 #        except Exception as e:
 #            print(f"An error occurred while interacting with the OpenAI API: {e}")
 #            return None
-
-    def ask_llm(self, question, user_id):
-##        user_type = self.db_manager.get_user_type(user_id)  # Retrieve user type from database
-        user_type = "student" if user_id == "kasutaja1" else "general"
+    
+    def ask_llm(self, question, user_id="kasutaja1"):
+##        user_type = self.db_manager.get_user_type(user_id)  # Retrieve user type from database        
+        print(ele for ele in student_user_ids)
+        user_type = "student" if user_id in self.student_user_ids else "general"
+        print(f"Debug user_type: {user_type}")
+        #user_type = "student" if user_id == "kasutaja1" else "general"
 ##        
 ##        #messages = self.form_message(question) # user_type not specified
         messages = self.form_message(question, user_type=user_type)
@@ -79,12 +83,13 @@ class APIClient:
             #system_message = "You are a supportive teacher assisting 11-13 year-old children."
             #system_message = "Sa oled abivalmis õpetaja, kes aitab 11-13 aastaseid kooliõpilasi. Neile nõu andes lähtud sa aktiivse õppimise, aktiivse õppija ning probleemõppe metoodikast."
 #            system_message = "Hinda selle eesmärgi sobivust 11-13 aasta vanuse kooliõpilase jaoks. Anna tagasisidet ja soovitusi eesmärgi parandamiseks 11-13 aastasele kooliõpilasele. Ära anna käitumissoovitusi eesmärgi saavutamiseks."
-            system_message = ""
-        elif user_type == "professional":
-            #system_message = "Hinda selle eesmärgi sobivust 11-13 aasta vanuse kooliõpilase jaoks. Anna tagasisidet ja soovitusi eesmärgi parandamiseks 11-13 aastasele kooliõpilasele. Ära anna käitumissoovitusi eesmärgi saavutamiseks."
-            system_message = ""
-        else:
+            #system_message = ""
             system_message = "Sa oled 11-13 aastaste õpilaste haridusnõustaja. Sa loed 11-13 aastase õpilase poolt sõnastatud eesmärki koolis tegutsemiseks seitsme nädala jooksul. Anna konkreetseid, selgeid, lühidaid soovitusi selle eesmärgi parandamiseks. Kasuta soovituste andmiseks eesmärgi sõnastamise süsteemi 3R: Kas eesmärk on saavutatav? Kas sa usud, et sa suudad eesmärgi saavutada seitsme nädalaga? Kas selle eesmärgi saavutamine teeb sulle heameelt? Ära anna otseseid käitumissoovitusi eesmärgi saavutamiseks. Ara anna ise vastuseid nendele küsimustele, anna neid küsimusi kasutades tagasisidet õpilasele. Pöördu õpilase poole otse ja kasuta selleks otsest kõnet."
+        #elif user_type == "professional":
+            #system_message = "Hinda selle eesmärgi sobivust 11-13 aasta vanuse kooliõpilase jaoks. Anna tagasisidet ja soovitusi eesmärgi parandamiseks 11-13 aastasele kooliõpilasele. Ära anna käitumissoovitusi eesmärgi saavutamiseks."
+            #system_message = ""
+        #else:
+            #system_message = "Sa oled 11-13 aastaste õpilaste haridusnõustaja. Sa loed 11-13 aastase õpilase poolt sõnastatud eesmärki koolis tegutsemiseks seitsme nädala jooksul. Anna konkreetseid, selgeid, lühidaid soovitusi selle eesmärgi parandamiseks. Kasuta soovituste andmiseks eesmärgi sõnastamise süsteemi 3R: Kas eesmärk on saavutatav? Kas sa usud, et sa suudad eesmärgi saavutada seitsme nädalaga? Kas selle eesmärgi saavutamine teeb sulle heameelt? Ära anna otseseid käitumissoovitusi eesmärgi saavutamiseks. Ara anna ise vastuseid nendele küsimustele, anna neid küsimusi kasutades tagasisidet õpilasele. Pöördu õpilase poole otse ja kasuta selleks otsest kõnet."
             #system_message = "Sa loed 11-13 aasta õpilase poolt sõnastatud eesmärki koolis tegutsemiseks seitsme nädala jooksul. Anna konkreetseid, selgeid, lühidaid soovitusi selle eesmärgi parandamiseks. Kasuta tagasiside andmiseks eesmärgi sõnastamise süsteemi ABCDE: Kas eesmärk on saavutatav seitsme nädala jooksul koolis? Kas sa usud, et sa suudad eesmärgi saavutada? Kas sa oled valmis pingutama eesmärgi saavutamiseks? Kas sa oled oma eesmärgi kirja pannud? Kas see eesmärk teeb sind energiliseks ja teotahteliseks? Ära anna käitumissoovitusi eesmärgi saavutamiseks. Ara anna ise vastuseid nendele küsimustele, anna neid küsimusi kasutades tagasisidet õpilasele. Pöördu tema poole otse ja kasuta selleks otsest kõnet."
             #system_message = "Anna konkreetseid, selgeid, lühidaid soovitusi eesmärgi sõnastamiseks 11-13 aastasele kooliõpilasele järgmiseks seitsmeks nädalaks. Kasuta eesmärgi sõnastamise süsteemi GROW: Mida sa tahad saavutada? Mida sa praegu oskad, tead ja teed selle eesmärgi suhtes? Mida sa saaksid teha, et oma eesmärki saavutada? Mida konkreetselt sa teed? Ära anna käitumissoovitusi eesmärgi saavutamiseks. Pöördu tema poole otse ja kasuta selleks otsest kõnet."
             #system_message = "Anna konkreetseid, selgeid, lühidaid soovitusi eesmärgi sõnastamiseks 11-13 aastasele kooliõpilasele järgmiseks seitsmeks nädalaks. Kasuta eesmärgi sõnastamise süsteemi OKR: Mida sa tahad saavutada? Kuidas sa mõõdad enda edenemist ja saad aru, et oled oma eesmärgile lähemale liikunud või selle saavutanud? Ära anna käitumissoovitusi eesmärgi saavutamiseks. Pöördu tema poole otse ja kasuta selleks otsest kõnet."
